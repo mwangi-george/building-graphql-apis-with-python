9f4d0e429d419025fe99cbcfa5cd6f75af9f786a
mwangi-george <mwangigeorge648@gmail.com>
2025-08-01 09:33:29 +0300

    Hello world application (entry point)

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from building-graphql-apis-with-python!")
+from graphene import Schema, ObjectType, String
+from loguru import logger
 
 
-if __name__ == "__main__":
-    main()
+# Root Class of the schema
+class Query(ObjectType):
+    hello = String(name=String(default_value="world"))
+
+    # resolver function: responsible for fetching and returning the data for a particular field
+    def resolve_hello(self, info, name):
+        logger.info(f"Resolving hello for name: {name}")
+        return f"Hello, {name}!"
+
+# instance of the Schema
+schema = Schema(query=Query)
+
+
+# query document
+gql = """
+{
+    hello(name: "GraphQL")
+}
+"""
+
+if __name__ == '__main__':
+    result = schema.execute(gql)
+    print(result)
